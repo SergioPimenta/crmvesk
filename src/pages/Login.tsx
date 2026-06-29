09f0e68ba02ab76225848f6de7659cca92f9cd2c
Sergio Pimenta <sergiopimentaajr@gmail.com>
2026-06-29 19:17:16 -0300

    Ajustes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -50,7 +50,7 @@ const Login = () => {
         <div className="vesk-login-header">
           <div className="vesk-login-logo">
             <i className="ti ti-bolt" style={{ color: 'var(--vesk-orange)', fontSize: 24 }} aria-hidden="true" />
-            VESK <span>CRM</span>
+            VESK <span>CR</span>
           </div>
           <p className="vesk-login-sub">Entre com suas credenciais para acessar o painel</p>
         </div>
